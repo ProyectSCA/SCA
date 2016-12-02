fb8abec2927c4cf48fa81b47d1ebd7c4b32f6c0b
marlonji2360 <marlonji2360@gmail.com>
2016-12-02 09:58:58 -0600

    Se realizo clase Sistema Operativo y Modelo Sistema Operativo

---
+++ SCA/SCA/Clases/Beans/SistemaOperativo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SCA.Clases.Beans
+{
+    public class SistemaOperativo
+    {
+        private int id_so;
+        private String nombre;
+
+        public int Id_so
+        {
+            get
+            {
+                return id_so;
+            }
+
+            set
+            {
+                id_so = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+
+            set
+            {
+                nombre = value;
+            }
+        }
+    }
+}
---
+++ SCA/SCA/Clases/Modelos/ModeloSistemaOperativo.cs
@@ -0,0 +1,33 @@
+﻿using SCA.Clases.Beans;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace SCA.Clases.Modelos
+{
+    public class ModeloSistemaOperativo
+    {
+        private Conexion c;
+
+        public ModeloSistemaOperativo()
+        {
+            c = new Conexion();
+        }
+        //Metodo para crear Sistema Operativo
+        public void crearSO(SistemaOperativo so)
+        {
+            String insert = "EXEC ingreso_so '" + so.Nombre + "'";
+            ejecutar(insert);
+        }
+        //Metodo que ejecuta Insert, Update, Delete
+        private void ejecutar(String consultaSQL)
+        {
+            c.con.Open();
+            c.sen = new SqlCommand(consultaSQL, c.con);
+            c.sen.ExecuteNonQuery();
+            c.con.Close();
+        }
+    }
+}
