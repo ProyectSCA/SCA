2cc0700f110f236b5c343835172d2f7e95c5115d
marlonji2360 <marlonji2360@gmail.com>
2016-12-05 12:30:46 -0600

    Agregamos Clase y Modelo MarcaComputadoras

---
+++ SCA/SCA/Clases/Beans/MarcaComputadora.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SCA.Clases.Beans
+{
+    public class MarcaComputadora
+    {
+        private int id_marca;
+        private string marca;
+
+        public int Id_marca
+        {
+            get
+            {
+                return id_marca;
+            }
+
+            set
+            {
+                id_marca = value;
+            }
+        }
+
+        public string Marca
+        {
+            get
+            {
+                return marca;
+            }
+
+            set
+            {
+                marca = value;
+            }
+        }
+    }
+}
---
+++ SCA/SCA/Clases/Modelos/ModeloMarcaComputadora.cs
@@ -0,0 +1,36 @@
+﻿using SCA.Clases.Beans;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace SCA.Clases.Modelos
+{
+    public class ModeloMarcaComputadora
+    {
+        private Conexion c;
+
+        public ModeloMarcaComputadora()
+        {
+            c = new Conexion();
+        }
+
+        public Boolean crearMarcaComputadora(MarcaComputadora mc)
+        {
+            String insert = "EXEC ingreso_marcacomputadora '"+mc.Marca+"'";
+            ejecutar(insert);
+
+
+            return true;
+        }
+
+        private void ejecutar(string insert)
+        {
+            c.con.Open();
+            c.sen = new SqlCommand(insert, c.con);
+            c.sen.ExecuteNonQuery();
+            c.con.Close();
+        }
+    }
+}
